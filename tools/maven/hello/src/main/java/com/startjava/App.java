f35996f7939c1e7b64494402bd9aea888853e8f0
henry <leakeyash@163.com>
2018-09-05 13:08:47 +0800

    add jar dependency

--- tools/maven/hello/src/main/java/com/startjava/App.java
+++ tools/maven/hello/src/main/java/com/startjava/App.java
@@ -6,8 +6,10 @@ package com.startjava;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ClassNotFoundException
     {
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("初始化驱动成功");
         System.out.println( "Hello World!" );
     }
 }
