d8bca3032938da7ccc1b9042648c46538efb04d8
henry li <leakeyash@163.com>
2018-12-19 19:44:50 +0800

    extract method

--- core/intermediate/src/io/FileObject.java
+++ core/intermediate/src/io/FileObject.java
@@ -1,39 +1,29 @@
 package io;
 
-import com.sun.deploy.util.StringUtils;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
 /**
  * @author henry
  */
-public class FileTest {
+public class FileObject {
     public static void main(String[] args) throws IOException {
         String file = "F:\\test\\henry\\hawk\\a.txt";
         String dir = new File(file).getParent();
         createNewFile(file);
         createFiles(dir);
         createFiles(Paths.get(dir,"sub").toString());
+        List<File> files = getAllFiles(dir);
+        printMaxMinFileNames(files);
+    }
+
+    private static void printMaxMinFileNames(List<File> files) {
         String maxName = null;
         String minName = null;
-        List<File> files = new ArrayList<>();
-        Stack<File> stack = new Stack<>();
-        stack.push(Paths.get(dir).toFile());
-        while(!stack.isEmpty()) {
-            File child = stack.pop();
-            if (child.isDirectory()) {
-                for(File f : child.listFiles()) {
-                    stack.push(f);
-                }
-            } else if (child.isFile()) {
-                files.add(child);
-            }
-        }
+
         long min =Long.MAX_VALUE;
         long max =0;
         for(File item: files){
@@ -48,6 +38,23 @@ public class FileTest {
         }
         System.out.println("Max name: "  + maxName +", max:"+max);
         System.out.println("Min name: "  + minName +", min:"+min);
+    }
+
+    private static List<File> getAllFiles(String dir) {
+        List<File> files = new ArrayList<>();
+        Stack<File> stack = new Stack<>();
+        stack.push(Paths.get(dir).toFile());
+        while(!stack.isEmpty()) {
+            File child = stack.pop();
+            if (child.isDirectory()) {
+                for(File f : Objects.requireNonNull(child.listFiles())) {
+                    stack.push(f);
+                }
+            } else if (child.isFile()) {
+                files.add(child);
+            }
+        }
+        return files;
     }
 
     private static boolean createNewFile(String file) throws IOException {
