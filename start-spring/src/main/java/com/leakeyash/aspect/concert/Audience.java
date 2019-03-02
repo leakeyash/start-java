da85793f0b2f3516a7985235ae7f5d0b7db542d4
henry <leakeyash@163.com>
2019-03-02 16:49:03 +0800

    change to point cut

--- start-spring/src/main/java/com/leakeyash/aspect/concert/Audience.java
+++ start-spring/src/main/java/com/leakeyash/aspect/concert/Audience.java
@@ -3,22 +3,29 @@ package com.leakeyash.aspect.concert;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author henry
+ */
 @Aspect
 @Component
 public class Audience {
-    @Before("execution(** com.leakeyash.aspect.concert.Performance.perform(..))")
+
+    @Pointcut("execution(** com.leakeyash.aspect.concert.Performance.perform(..))")
+    public void performance(){
+    }
+    @Before("performance()")
     public void silenceCellPhones(){
         System.out.println("Silencing cell phones");
     }
-    @Before("execution(** com.leakeyash.aspect.concert.Performance.perform(..))")
+    @Before("performance()")
     public void takeSeats(){
         System.out.println("Taking seats");
     }
-    @AfterReturning("execution(** com.leakeyash.aspect.concert.Performance.perform(..))")
+    @AfterReturning("performance()")
     public void applause(){
         System.out.println("CLAP CLAP CLAP!!!");
     }
-    @AfterThrowing("execution(** com.leakeyash.aspect.concert.Performance.perform(..))")
+    @AfterThrowing("performance()")
     public void demandRefund(){
         System.out.println("Demanding a refund");
     }
